fb8e455fc6d24f01e835b2de43ac8359b9d03c01
Rekaterina <rkatherine0602@gmail.com>
2019-10-13 09:05:15 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+    let arr = expression.split('*');
+    let five = 0;
+    let two = 0;
+    let n;
+    let number;
+    
+      arr.map((i) => {
+          if (i.includes('!!')) {
+              n = 2;
+              number = i.slice(0, -2);
+          } else {
+              n = 1;
+              number = i.slice(0, -1);
+          }
+          
+          for (let j = 0; j < number; number -= n) {
+              let result = number;  
+  
+              while (result % 2 === 0) {
+                  two++;
+                  result = result / 2;
+              }
+              
+              while (result % 5 === 0) {
+                  five++;
+                  result = result / 5;
+              }
+          }
+      });
+  
+      return Math.min(two, five);
+  }
